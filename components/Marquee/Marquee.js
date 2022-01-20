12398304d4cf9074f125f38f62a42c9a2519a10b
Darren <hello@bigpepperdesign.com>
2022-01-20 14:41:19 +0000

    Home Marquee

--- components/Marquee/Marquee.js
+++ components/Marquee/Marquee.js
@@ -2,7 +2,6 @@ import Script from "next/script";
 
 const Marquee = () => {
   return (
-    <>
     <section className="marquee overflow-hidden relative font-display text-[50px] lg:text-[140px] leading-none lg:leading-none">
         <div className="marquee__inner w-fit flex flex-row" aria-hidden="true">
             <div className="marquee__part  shrink-0">
@@ -24,10 +23,8 @@ const Marquee = () => {
             craft.  <em>entertainment</em>.  art. events.  music.
             </div>
         </div>
-    </section>
-
-    <Script id="Marquee" strategy="afterInteractive" src="/js/marquee.js"/>
-    </>)
+        <Script id="Marquee" strategy="afterInteractive" src="/js/marquee.js"/>
+    </section>)
 }
 
 export default Marquee;
