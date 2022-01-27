4f3c64ebc1a7ceb12021f46fb68d5effe8e55fe9
Arrann Diamond <arrann@pixello.co.uk>
2022-01-27 12:57:59 +0000

    Updated CTA section

--- components/Ctas/Ctas.js
+++ components/Ctas/Ctas.js
@@ -4,15 +4,15 @@ const Ctas = () => {
   return (
     <section>
         <div className="lg:flex">
-        <Cta 
-            copy="Looking to host a team away day? Somewhere for a workshop? We’ve got lots of spaces for you across our hotel."
+        <Cta
+            copy="Looking to host a team away day in central London or the St. Pauls area? Need somewhere for a workshop, client meeting or gathering? We've got lots of spaces for you across our hotel to suit your requirements and the perfect backdrop for a productive and successful meeting."
             title="Need Space"
             img="/image/del/cta/cta1.jpg"
             btnTitle="Enquire" btnUrl="#" />
-        <Cta 
+        <Cta
         bgClass="bg-blue-1"
         textColor="text-white"
-        copy="Celebrate occasions big and small, with our private spaces. You just bring your glam-rags, and we’ll sort the rest."
+        copy="Celebrate occasions big and small, with our private spaces. Whatever the occasion we have the ideal function rooms for your event. You just bring your glam-rags, and we'll sort the rest."
         title="Let’s Partaay"
         img="/image/del/cta/cta2.jpg"
         btnTitle="Enquire" btnUrl="#" />
