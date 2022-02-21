e7599e22861df82be3a6c3a6ce9907ecef687fc1
Darren Powell <hello@bigpepperdesign.com>
2022-02-21 13:59:56 +0000

    Contact form / Performance

--- components/Contact/ModuleContact.js
+++ components/Contact/ModuleContact.js
@@ -2,36 +2,70 @@ import { useState } from "react";
 
 const ModuleContact = ({settings}) => {
 
-    const [enquiry, setEnquiry] = useState('')
+    const [hp1, setHp1] = useState('')
+    const [hp2, setHp2] = useState('hp')
+    const [enquiry, setEnquiry] = useState('General Enquiry')
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
     const [submitted, setSubmitted] = useState(false)
+    const [thankyou, setThankyou] = useState(false)
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
 
         let data = {
             enquiry,
-            email,
+            yourEmail: email,
             message
         }
 
-        fetch('/api/contact', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }).then((res) => {
+        if ( hp1 !== '' || hp2 !== 'hp' ) {
 
-            if (res.status === 200) {
-                setSubmitted(true)
-                setEmail('')
-                setMessage('')
-                setEnquiry('')
+            return;
+        }
+
+        const result = await fetch('/api/contact', {
+            method: 'POST',
+            
+            body: JSON.stringify(data)
+        }).then((data) => {  
+            
+            if (data.status === 200) {
+
+                return data.json();
             }
         });
+
+        if ( result.result.status == 'mail_sent' ) {
+            setSubmitted(true)
+            setEmail('')
+            setMessage('')
+            setEnquiry('')
+        } else {
+
+            const form_groups = document.querySelectorAll('.form-group');
+            form_groups.forEach( (group) => {
+                group.classList.remove('text-red-1');
+
+                if ( group.querySelector('.error') ) {
+                    group.querySelector('.error').remove();
+                }
+            });
+
+            if ( result.result.invalid_fields.length ) {
+                
+
+                result.result.invalid_fields.forEach( (invalid_field) => {
+                    const el = document.getElementById(invalid_field.error_id);
+                    el.classList.add('text-red-1');
+                    
+                    el.insertAdjacentHTML('beforeend', '<span class="absolute text-12px error">'+invalid_field.message+'</span>');
+
+                });         
+            }
+
+        }
+
     }
 
     return (
@@ -47,23 +81,27 @@ const ModuleContact = ({settings}) => {
 
                                 <p className={`transition-all delay-500 absolute my-10 uppercase ${submitted ? 'opacity-100 visible' : 'opacity-0 invisible'} `}>We look forward to finding your message.</p>
                                 <form className={`transition-all ${submitted ? 'opacity-0 invisible' : ''} form`}>
-                                    <div className="form-group">
+                                    <div className="form-group" id="-ve-enquiry">
                                         <label className="hidden" htmlFor='enquiry'>Enquiry Type</label>
                                         <select type='text' name='enquiry'
                                         onChange={(e)=>{setEnquiry(e.target.value)}}>
                                             <option>General Enquiry</option>
                                         </select>
                                     </div>
-                                    <div className="form-group">
+                                    <div className="form-group" id="-ve-yourEmail">
                                         <label className="hidden" htmlFor='email'>Email</label>
-                                        <input placeholder="Enter your email" type='email' name='email'
+                                        <input placeholder="Enter your email" type='email' name='your-email'
                                         onChange={(e)=>{setEmail(e.target.value)}} />
                                     </div>
-                                    <div className="form-group">
+                                    <div className="form-group" id="-ve-message">
                                         <label className="hidden" htmlFor='message'>Message</label>
                                         <textarea placeholder="Message" type='text' name='message'
                                         onChange={(e)=>{setMessage(e.target.value)}}></textarea>
                                     </div>
+                                    <input className="!hidden" placeholder="Enter your email" type='text' name='hp1' value={hp1}
+                                        onChange={(e)=>{setHp1(e.target.value)}} />
+                                    <input className="!hidden" placeholder="Enter your email" type='text' name='hp2' value={hp2}
+                                        onChange={(e)=>{setHp2(e.target.value)}} />
                                     <button className="button-tertiary" type='submit' onClick={(e)=>{handleSubmit(e)}}>Submit</button>
                                 </form >
 
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,36 +1,47 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function (req, res) {
+export default async function (req, res) {
   //res.status(200).json({ name: 'John Doe' });
 
-  require('dotenv').config()
-  
-  let nodemailer = require('nodemailer');
-  
-  const transporter = nodemailer.createTransport({
-    port: 587,
-    host: "mail.bigpepperdesign.com",
-    auth: {
-      user: "darren@bigpepperdesign.com",
-      pass: "sw4Y.yfBz1",
-    },
-    secure: false,
-  })
-  
-  const mailData = {
-    from: 'hello@bigpepperdesign.com',
-    to: 'dazpowell@gmail.com',
-    subject: `Message From ${req.body.name}`,
-    text: req.body.message + " | Sent from: " + req.body.email,
-    html: `<div>${req.body.message}</div><p>Sent from:
-    ${req.body.email}</p>`
+  function convertJsontoUrlencoded(obj) {
+    let str = [];
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        str.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
+      }
+    }
+    return str.join('&');
   }
-  transporter.sendMail(mailData, function (err, info) {
-    if(err)
-      console.log(err)
-    else
-      console.log(info)
-  })
-  res.status(200).json({message: 'Done'})
+
+  require('dotenv').config()
+
+  const API_URL = process.env.API_URL;
+  const WP_CF7_CONTACTID = process.env.WP_CF7_CONTACTID;
+
+
+  if ( API_URL && WP_CF7_CONTACTID ) {
+    const WP_CF7_FEEDBACK = API_URL+'/wp-json/contact-form-7/v1/contact-forms/'+WP_CF7_CONTACTID+'/feedback';
+
+
+    const body = JSON.parse(req.body);
+
+    const result = await fetch(WP_CF7_FEEDBACK, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+      },
+      body: convertJsontoUrlencoded(body)
+    }).then((data) => {
+      if (data.status === 200) {
+        return data.json();
+      }
+    });
+
+    res.status(200).send({result})
+  } else {
+    res.status(400).json({message: 'Form Error'})
+  }
+  
+  
 
 }
