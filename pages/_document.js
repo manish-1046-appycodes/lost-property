c2835441791f9ade71ceef79c442cf5e40a2bde9
Manish Sharma <manish@appycodes.com>
2023-04-27 17:40:49 +0530

    fix:mailchimp initial load at 5seconds, feat:add post code to address

--- pages/_document.js
+++ pages/_document.js
@@ -10,21 +10,19 @@ class MyDocument extends Document {
     return (
       <Html lang="en-GB">
         <Head>
-          <script
+          {/* <script
+            defer
+            async
             type="text/javascript"
-            id="mcjs"
-            dangerouslySetInnerHTML={{
-              __html: `<script src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
-              `,
-            }}
-          />
+            src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
+          /> */}
 
           {/* mailchimp script */}
           <script
             type="text/javascript"
             id="mcjs"
             dangerouslySetInnerHTML={{
-              __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/e531d17fe22bad56eb6bd5377/adbb95630e50cc39579bb1a21.js");`,
+              __html: `setTimeout(()=>{!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/e531d17fe22bad56eb6bd5377/${process.env.NEXT_PUBLIC_MAILCHIMP_ID}.js");},5000);`,
             }}
           />
         </Head>
