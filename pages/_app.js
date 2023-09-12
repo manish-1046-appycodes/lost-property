e34b016438e885b4dfce9883410dacecf02a371a
Pratik <pratik@appycodes.com>
2023-09-12 12:26:39 +0530

    fix: rebasing

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
-import '../styles/style.scss'
-import '../styles/react-date-range/styles.css'; // main css file
-import '../styles/react-date-range/theme/default.css'; // theme css file
+import { useEffect } from "react";
+import { initGTM } from "../gtm"; // Import the GTM configuration
+import "../styles/style.scss";
+import "../styles/react-date-range/styles.css"; // main css file
+import "../styles/react-date-range/theme/default.css"; // theme css file
 
 function MyApp({ Component, pageProps }) {
+  // Initialize GTM when the app mounts
+  useEffect(() => {
+    initGTM();
+  }, []);
 
-  const getLayout = Component.getLayout || ((page) => page)
+  const getLayout = Component.getLayout || ((page) => page);
 
-  return (
-    getLayout(<Component {...pageProps} />)
-  )
+  return getLayout(<Component {...pageProps} />);
 }
 
-export default MyApp
+export default MyApp;
